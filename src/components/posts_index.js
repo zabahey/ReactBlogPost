7849120716439ff4b66c490ae39ccdefa3a662f0
Parinya Samrongsap <zand.p@excelbangkok.com>
2018-03-02 14:28:09 +0700

    adjust component name

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 
-export default class PostIndex extends Component {
+class PostsIndex extends Component {
     render() {
         return (
             <div>Posts Index</div>
         );
     }
 }
+
+export default PostsIndex;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from "react-router-dom";
 
 import reducers from './reducers';
-import PostIndex from "./components/posts_index";
+import PostsIndex from "./components/posts_index";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -13,7 +13,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostIndex} />
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>
