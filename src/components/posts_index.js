9f7b9a6b0b8aa592c65f5b6f97b0bc11c72398b2
Parinya Samrongsap <zand.p@excelbangkok.com>
2018-03-02 14:09:01 +0700

    define first route for app

---
+++ src/components/posts_index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class PostIndex extends Component {
+    render() {
+        return (
+            <div>Posts Index</div>
+        );
+    }
+}
