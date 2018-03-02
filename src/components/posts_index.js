8f25625eeaf7013c918f80994b812ad4732e60c0
Parinya Samrongsap <zand.p@excelbangkok.com>
2018-03-02 15:23:49 +0700

    fetch posts from server after component rendered

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -3,6 +3,10 @@ import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
 
 class PostsIndex extends Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
     render() {
         return (
             <div>Posts Index</div>
