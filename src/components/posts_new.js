39f357e3b1b755152299e0a3df28b37b4276c5df
Parinya Samrongsap <zand.p@excelbangkok.com>
2018-03-05 13:47:02 +0700

    render title field for Field component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,9 +2,27 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
+    renderTitleField(field) {
+        // ...field.input is object which contains a bunch of event handler
+        // and a bunch of differnt props e.g. onChange onBlur on Focus and value of hte input
+        return (
+            <div>
+                <input
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
     render() {
         return(
-            <div></div>
+            <form>
+                <Field 
+                    name="title"
+                    component={this.renderTitleField}
+                />
+            </form>
         );
     }
 }
