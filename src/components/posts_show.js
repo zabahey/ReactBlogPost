ef097946754cdbecc02593b0d03fa0e0fdfca249
zand <zabahey.sukoi@gmail.com>
2018-03-05 19:54:02 +0700

    select only interesting posts inside mapStateToProps by ownProps

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -3,6 +3,12 @@ import { connect } from "react-redux";
 import { fetchPost } from "../actions";
 
 class PostsShow extends Component {
+    componentDidMount() {
+        // come from wildcard in the url
+        const { id } = this.props.match.params;
+        this.props.fetchPost(id);
+    }
+
     render() {
         return (
             <div>
@@ -10,6 +16,11 @@ class PostsShow extends Component {
             </div>
         );
     }
+}
+
+//this.props === ownProps
+function mapStateToProps({ posts }, ownProps) {
+    return { post: posts[ownProps.match.params.id] };
 }
 
 export default connect(null, { fetchPost })(PostsShow);
