44d3901f8d9ef45b77ac1826ebcab10397c5a4fb
Parinya Samrongsap <zand.p@excelbangkok.com>
2018-03-05 13:59:08 +0700

    generalizing render input for Field component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
-    renderTitleField(field) {
+    renderField(field) {
         // ...field.input is object which contains a bunch of event handler
         // and a bunch of differnt props e.g. onChange onBlur on Focus and value of hte input
         return (
-            <div>
+            <div className="form-group">
+                <label htmlFor={field.input.name}>{field.label}</label>
                 <input
+                    className="form-control"
                     type="text"
                     {...field.input}
                 />
@@ -18,9 +20,20 @@ class PostsNew extends Component {
     render() {
         return(
             <form>
-                <Field 
+                <Field
+                    label="Title for Post" 
                     name="title"
-                    component={this.renderTitleField}
+                    component={this.renderField}
+                />
+                <Field
+                    label="Tags" 
+                    name="tags"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post Content" 
+                    name="content"
+                    component={this.renderField}
                 />
             </form>
         );
