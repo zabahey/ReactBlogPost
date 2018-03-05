7bc56ab8348b402397d8e724eaca76bd8f57fb11
zand <zabahey.sukoi@gmail.com>
2018-03-05 19:28:44 +0700

    receive new posts

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,17 @@
 import _ from "lodash";
-import { FETCH_POSTS } from "../actions";
+import { FETCH_POSTS, FETCH_POST } from "../actions";
 
 export default function(state = {}, action) {
     switch (action.type) {
+        case FETCH_POST:
+            // don't want to throw away existing post
+            // const post = action.payload.data;
+            // const newState =  { ...state };
+            // newState[post.id] = post;
+            // return newState;
+
+            // ES6
+            return { ...state, [action.payload.data.id]: action.payload.data}
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
         default:
